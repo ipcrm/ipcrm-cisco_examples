2f497d83ca2e4b9a9cadedfa393d7e7d06a8e8a3
ipcrm <mcadorette@puppetlabs.com>
2016-07-09 11:36:57 -0400

    updated

--- manifests/interfaces.pp
+++ manifests/interfaces.pp
@@ -1,24 +1,18 @@
 class cisco_examples::interfaces {
-
-  cisco_interface { 'ethernet1/9':
-    ensure          => 'present',
-    access_vlan     => '2',
-    duplex          => 'auto',
-    mtu             => '1500',
-    negotiate_auto  => true,
-    shutdown        => false,
-    speed           => 'auto',
-    stp_bpdufilter  => true,
-    stp_bpduguard   => true,
-    switchport_mode => 'access',
-    switchport_vtp  => false,
+  [3,4,5,6].each |$i| {
+    cisco_interface { "ethernet1/${i}":
+      ensure          => 'present',
+      description     => "Eth1/${i} Example Int Config",
+      access_vlan     => '2',
+      duplex          => 'auto',
+      mtu             => '1500',
+      negotiate_auto  => true,
+      shutdown        => false,
+      speed           => 'auto',
+      stp_bpdufilter  => true,
+      stp_bpduguard   => true,
+      switchport_mode => 'access',
+      switchport_vtp  => false,
+    }
   }
-
-  port_channel{'port channel example':
-    ensure       => present,
-    id           => 100,
-    interfaces   => ['ethernet1/1','ethernet1/2'],
-  }
-
-
 }
---
+++ manifests/portchannel.pp
@@ -0,0 +1,21 @@
+class cisco_examples::portchannel {
+  # Source https://github.com/cisco/cisco-network-puppet-module/blob/master/examples/cisco/demo_portchannel.pp
+  cisco_portchannel_global { 'default':
+    bundle_hash   => 'ip',
+    bundle_select => 'src-dst',
+    concatenation => true,
+    resilient     => false,
+    rotate        => '4',
+    symmetry      => false,
+  }
+
+  cisco_interface_portchannel {'port-channel100':
+    ensure                    => 'present',
+    lacp_graceful_convergence => false,
+    lacp_max_bundle           => 10,
+    lacp_min_links            => 2,
+    lacp_suspend_individual   => false,
+    port_hash_distribution    => 'adaptive',
+    port_load_defer           => true,
+  }
+}
